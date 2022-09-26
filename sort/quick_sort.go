f7d9c685e7b7c6c7e96135e1848fac3553e78fbb
stopworldgp <guopeng_huang@163.com>
2022-09-26 20:22:17 +0800

    add 快排

---
+++ sort/quick_sort.go
@@ -0,0 +1,46 @@
+package sort
+
+import "math/rand"
+
+//QuickSort 快排算法
+func QuickSort(nums []int) []int {
+	sort(nums, 0, len(nums)-1)
+	return nums
+}
+
+func sort(nums []int, left, right int) {
+	//1.基础条件
+	if left >= right {
+		return
+	}
+	//2.递归条件
+	pivot := partition(nums, left, right)
+	sort(nums, left, pivot)
+	sort(nums, pivot+1, right)
+}
+
+func partition(nums []int, left, right int) int {
+	pivot := left + rand.Intn(right-left)
+	pivotValue := nums[pivot]
+	for left <= right {
+		for nums[left] < pivotValue {
+			left++
+		}
+		for nums[right] > pivotValue {
+			right--
+		}
+		if left == right {
+			break
+		}
+		//swap
+		if left < right {
+			tmp := nums[left]
+			nums[left] = nums[right]
+			nums[right] = tmp
+			left++
+			right--
+		}
+	}
+	//通过上述左右转移，right为左区最右侧下标
+	return right
+}
--- sort/sort_test.go
+++ sort/sort_test.go
@@ -9,3 +9,8 @@ func TestSelectionSort(t *testing.T) {
 	nums := []int{10, 3, 1, 3, 5, 8}
 	fmt.Println(SelectionSort(nums))
 }
+
+func TestQuickSort(t *testing.T) {
+	nums := []int{5, 2, 3, 1}
+	fmt.Println(QuickSort(nums))
+}
