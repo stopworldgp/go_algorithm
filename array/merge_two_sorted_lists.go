26aa811036f1b31620958413f41c1ef7bb8fa101
stopworldgp <guopeng_huang@163.com>
2022-10-06 17:51:09 +0800

    合并两个有序链表

--- array/merge_two_sorted_lists.go
+++ array/merge_two_sorted_lists.go
@@ -7,9 +7,11 @@ type ListNode struct {
 
 //MergeTwoLists 21. 合并两个有序链表-https://leetcode.cn/problems/merge-two-sorted-lists/
 func MergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
+	//0.建立虚拟头结点
 	dummyHead := &ListNode{}
 	p := dummyHead
 
+	//1. 场景1 双链表不为空比较
 	for list1 != nil && list2 != nil {
 
 		if list1.Val < list2.Val {
@@ -21,7 +23,7 @@ func MergeTwoLists(list1 *ListNode, list2 *ListNode) *ListNode {
 		}
 		p = p.Next
 	}
-
+	//2. 场景2 其中一个链表为空，包含了 两种，一种开始一个链表为空和第二种 两个链表不一边长
 	if list1 != nil {
 		p.Next = list1
 	}
