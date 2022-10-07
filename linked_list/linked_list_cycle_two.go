ff606b284b0aaa0421307a91ba72d7c84e76a6c9
stopworldgp <guopeng_huang@163.com>
2022-10-07 12:02:14 +0800

    142. 环形链表

--- linked_list/linked_list_cycle_two.go
+++ linked_list/linked_list_cycle_two.go
@@ -1,12 +1,27 @@
 package linked_list
 
-//DetectCycle c142. 环形链表 II-https://leetcode.cn/problems/linked-list-cycle-ii/
+//DetectCycle 142. 环形链表 II-https://leetcode.cn/problems/linked-list-cycle-ii/
 func DetectCycle(head *ListNode) *ListNode {
+	fast, slow := head, head
 
-	//0. q = quick point,s = slow quick
-	q, s := head, head
-	for q != nil && q != s {
-
+	//0. 快慢指针第一次相遇，注意判断 fast 和fast.next 都不能为空
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		//1. 当相遇时跳出，不能将该判断放到for里，因为第一次 fast= slow = head
+		if fast == slow {
+			break
+		}
+	}
+	if fast == nil || fast.Next == nil {
+		return nil
 	}
 
+	slow = head
+	//1. 将slow指针放到head处，两个指针开始从相同步数开始走
+	for fast != slow {
+		slow = slow.Next
+		fast = fast.Next
+	}
+	return slow
 }
