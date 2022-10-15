59b72138bdd8f171ba1691db024f9283fd62032e
stopworldgp <guopeng_huang@163.com>
2022-10-15 16:31:09 +0800

    27. 移除元素

---
+++ array/remove_element.go
@@ -0,0 +1,17 @@
+package array
+
+//27. 移除元素-https://leetcode.cn/problems/remove-element/
+func removeElement(nums []int, val int) int {
+	if len(nums) == 0 {
+		return 0
+	}
+	slow, fast := 0, 0
+	for fast < len(nums) {
+		if nums[fast] != val {
+			nums[slow] = nums[fast]
+			slow++
+		}
+		fast++
+	}
+	return slow
+}
